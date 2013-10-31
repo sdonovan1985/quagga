1f7ecd4afb65c9ea65cf6200daea4d5b182419f5
Sean Donovan <sdonovan@gatech.edu>
2013-10-31 12:54:01 -0400

    Purged old code. Compiles more better now.

--- quagga-0.99.21/bgpd/bgp_sdxext.c
+++ quagga-0.99.21/bgpd/bgp_sdxext.c
@@ -179,10 +179,7 @@ sdxext_allocate_connection(void)
     cxn->recv_sequence = 1;
     cxn->send_sequence = 1;
     cxn->version = PROTOCOL_VERSION;
-    cxn->secret = SUPERSECRETLOGINSECRET;
     cxn->missed_keepalives = 0;
-    cxn->buffer_to_send = NULL;
-    cxn->update_list_to_send = NULL;
 
     return NULL;
 }
@@ -196,17 +193,6 @@ sdxext_free_connection(struct sdx_bgp_connection* cxn)
 
     if (NULL == cxn)
         return;
-    // free up any allocated memory
-    if (NULL != cxn->buffer_to_send)
-    {
-        free(cxn->buffer_to_send);
-        cxn->buffer_to_send = NULL;
-    }
-    if (NULL != cxn->update_list_to_send)
-    {
-        free(cxn->update_list_to_send);
-        cxn->update_list_to_send = NULL;
-    }
 
     // stop any timer
     memset(&zero_time, 0, sizeof(struct itimerspec));
@@ -245,430 +231,15 @@ sdxext_restart_cxn_timer(struct sdx_bgp_connection* cxn)
 }
 
 int
-sdxext_get_pdu_to_send_resources(struct sdx_bgp_connection* cxn,
-                                 struct sdx_bgp_pdu* pdu,
-                                 size_t pdu_specifics_size,
-                                 void* pdu_specifics,
-                                 size_t update_buffer_size,
-                                 void* update_buffer)
-{
-    if (NULL == cxn)
-        return -1;
-    // Set pdu pointer
-    pdu = &(cxn->pdu_to_send);
-
-    // initialize PDU
-    memset(pdu, 0, sizeof(struct sdx_bgp_pdu));
-
-    // Allocate pdu_specifics if there's a specified size
-    if (0 != pdu_specifics_size)
-    {
-        pdu_specifics = malloc(pdu_specifics_size);
-        if (NULL == pdu_specifics)
-            return -2;
-        memset(pdu, 0, pdu_specifics_size);
-        // since they're in a union, doesn't matter which member it's set to
-        pdu->login_request = pdu_specifics;
-    }
-
-    // Allocate update_buffer if there's a specified size
-
-    if (0 != update_buffer_size)
-    {
-        update_buffer = malloc(update_buffer_size);
-        if (NULL == update_buffer)
-        {
-            if (0 != pdu_specifics_size)
-                free(pdu_specifics);
-            return -3;
-        }
-    }
-}
-
-int
-sdxext_free_sent_pdu_resources(struct sdx_bgp_connection* cxn)
-{
-    if (NULL == cxn)
-        return -1;
-    // free up any allocated memory
-    if (NULL != cxn->buffer_to_send)
-    {
-        free(cxn->buffer_to_send);
-        cxn->buffer_to_send = NULL;
-    }
-    if (NULL != cxn->update_list_to_send)
-    {
-        free(cxn->update_list_to_send);
-        cxn->update_list_to_send = NULL;
-    }
-    return 0;
-}
-
-int
-sdxext_register_AS(struct sdx_bgp_connection* cxn, int as)
-{
-    struct sdx_bgp_as_entry* as_entry;
-
-    if (sdxext_is_AS_registered(cxn, as))
-        return 1;
-
-    if ((NULL == cxn->as_list_head) &&
-        (NULL == cxn->as_list_tail))
-    {
-        cxn->as_list_head = malloc(sizeof(struct sdx_bgp_as_entry));
-        as_entry = cxn->as_list_head;
-        as_entry->prev = NULL;
-        as_entry->next = NULL;
-        as_entry->entry = as;
-        cxn->as_list_tail = as_entry;
-    }
-    else
-    {
-        cxn->as_list_tail->next = malloc(sizeof(struct sdx_bgp_as_entry));
-        as_entry = cxn->as_list_tail->next;
-        as_entry->prev = cxn->as_list_tail;
-        as_entry->next = NULL;
-        as_entry->entry = as;
-        cxn->as_list_tail = as_entry;
-    }
-
-    return 0;
-}
-
-int
-sdxext_deregister_AS(struct sdx_bgp_connection* cxn, int as)
-{
-    struct sdx_bgp_as_entry* as_entry;
-
-    if (NULL == cxn->as_list_head &&
-        NULL == cxn->as_list_tail)
-    {
-        return 1;
-    }
-
-    as_entry = cxn->as_list_head;
-    while (as_entry != NULL)
-    {
-        if (as == as_entry->entry)
-        {
-            struct sdx_bgp_as_entry* prev = as_entry->prev;
-            struct sdx_bgp_as_entry* next = as_entry->next;
-            prev->next = next;
-            next->prev = prev;
-            free(as_entry);
-            return 0;
-        }
-        as_entry = as_entry->next;
-    }
-    return 2;
-}
-
-int
-sdxext_is_AS_registered(struct sdx_bgp_connection* cxn, int as)
-{
-    struct sdx_bgp_as_entry* as_entry;
-
-    as_entry = cxn->as_list_head;
-    while (NULL != as_entry)
-    {
-        if (as == as_entry->entry)
-        {
-            return 1;
-        }
-        as_entry = as_entry->next;
-    }
-    return 0;
-}
-
-
-
-int
-sdxext_receive_PDU(struct sdx_bgp_pdu* pdu, struct sdx_bgp_connection* cxn)
-{
-    int ret = 0;
-
-
-    if (NULL == pdu)
-        return -1;
-
-    if (NULL == cxn)
-        return -2;
-
-    // verify correct connection_id
-    if (pdu->connection_id != cxn->connection_id)
-    {
-        sdxext_free_connection(cxn);
-        return -3;
-    }
-
-    // verify sequence and increment
-    if (pdu->send_sequence == cxn->recv_sequence)
-    {
-        cxn->recv_sequence++;
-    }
-    else
-    {
-        sdxext_free_connection(cxn);
-        return -4;
-    }
-
-    // Our sends - see if we can free up structures.
-    if (pdu->recv_sequence == cxn->send_sequence)
-    {
-        sdxext_free_sent_pdu_resources(cxn);
-        // If we've received something successfully, zero the missed count
-        cxn->missed_keepalives = 0;
-    }
-
-    // Validate opcode and send to processing functions
-    switch(pdu->opcode)
-    {
-        case SDX_BGP_LOGIN_REQUEST:
-            ret = sdxext_process_login_request(cxn, pdu);
-            break;
-        case SDX_BGP_KEEPALIVE:
-            ret = sdxext_process_keepalive(cxn, pdu);
-            break;
-        case SDX_BGP_REGISTER_AS:
-            ret = sdxext_process_register_as(cxn, pdu);
-            break;
-        case SDX_BGP_DEREGISTER_AS:
-            ret = sdxext_process_deregister_as(cxn, pdu);
-            break;
-        case SDX_BGP_REQUEST_ALL_ROUTES:
-            ret = sdxext_process_request_all_routes(cxn, pdu);
-            break;
-        case SDX_BGP_LOGIN_RESPONSE: // should not be seen on server-side
-        case SDX_BGP_UPDATE_LIST:    // should not be seen on server-side
-        default:
-            // assume the socket's sending garbage and close
-            sdxext_free_connection(cxn);
-            return -5;
-    }
-    // Close socket if there was a failure above or with cxn timer
-    if (ret < 0 ||
-        0 != sdxext_restart_cxn_timer(cxn))
-    {
-        sdxext_free_connection(cxn);
-    }
-
-    return ret;
-}
-
-
-
-/*
- * PDU processing functions
- */
-int
-sdxext_process_login_request(struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu)
-{
-    // check version
-    if (cxn->version != pdu->login_request->version)
-    {
-        return -1;
-    }
-
-    // verify secret
-    if (cxn->secret != pdu->login_request->secret)
-    {
-        return -2;
-    }
-
-    // build and send response
-    sdxext_build_and_send_login_response(cxn);
-
-    return 0;
-}
-
-int
-sdxext_process_keepalive(struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu)
-{
-    // send ack if this is not an ack
-    if (pdu->keepalive->ack_only == 0)
-        return sdxext_build_and_send_keepalive(cxn, 1);
-    return 0;
-}
-
-int
-sdxext_process_register_as (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu)
-{
-    // Add the AS to the watch list.
-    if (sdxext_register_AS(cxn, pdu->register_as->as))
-        return -1;
-    // Send ack
-    return sdxext_build_and_send_keepalive(cxn, 1);
-}
-
-int
-sdxext_process_deregister_as (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu)
-{
-    // Remove the AS from the watch list
-    if (sdxext_deregister_AS(cxn, pdu->register_as->as))
-        return -1;
-    // Send ack
-    return sdxext_build_and_send_keepalive(cxn, 1);
-}
-
-int
-sdxext_process_request_all_routes (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu)
-{
-//TODO
-    struct sdx_bgp_update* update_list = NULL;
-    size_t datasize = 0;
-    struct sdx_bgp_update* ul_ptr;
-    char* payload;
-    // Request all the routes
-    sdxext_fetch_all_routes(cxn, update_list);
-
-    // calculate output size
-    ul_ptr = update_list;
-    while (NULL != ul_ptr)
-    {
-        // for each update_list entry, sum up the size of all the parts
-
-
-        ul_ptr = ul_ptr->next;
-    }
-
-
-    // package them up to be sent.
-
-
-}
-
-//SPDSPD
-
-/*
- * BUILD AND SEND FUNCTIONS
- */
-
-
-int
-sdxext_build_and_send_login_response (struct sdx_bgp_connection* cxn)
-{
-    struct sdx_bgp_pdu* pdu = NULL;
-    struct sdx_bgp_login_response* lr = NULL;
-    // Allocate message on connection structure
-    if (0 != sdxext_get_pdu_to_send_resources(cxn, pdu,
-                                    sizeof(struct sdx_bgp_login_response),
-                                    lr,
-                                    0, NULL))
-    {
-        return -1;
-    }
-
-    // Set generic fields and increment correctly
-    pdu->opcode = SDX_BGP_LOGIN_RESPONSE;
-    pdu->send_sequence = cxn->send_sequence++;
-    pdu->recv_sequence = cxn->recv_sequence;
-    pdu->connection_id = cxn->connection_id;
-    pdu->size = sizeof(struct sdx_bgp_login_response);
-
-    // Set login response specific fields
-    lr->version = cxn->version;
-    lr->secret  = cxn->secret;
-
-    // Send packet
-    if (0 != sdxext_send_packet(cxn,
-            (void*)pdu,
-            sizeof(struct sdx_bgp_pdu) - sizeof(struct sdx_bgp_login_response*),
-            lr,
-            sizeof(struct sdx_bgp_login_response)))
-    {
-        return -2;
-    }
-}
-
-int
-sdxext_build_and_send_keepalive (struct sdx_bgp_connection* cxn,
-                                 int ack_only)
-{
-    struct sdx_bgp_pdu* pdu = NULL;
-    struct sdx_bgp_keepalive* ka = NULL;
-    // Allocate message on connection structure
-    if (0 != sdxext_get_pdu_to_send_resources(cxn, pdu,
-                                              sizeof(struct sdx_bgp_keepalive),
-                                              ka,
-                                              0, NULL))
-    {
-        return -1;
-    }
-    // Set generic fields and increment correctly
-    pdu->opcode = SDX_BGP_KEEPALIVE;
-    pdu->send_sequence = cxn->send_sequence++;
-    pdu->recv_sequence = cxn->recv_sequence;
-    pdu->connection_id = cxn->connection_id;
-    pdu->size = sizeof(struct sdx_bgp_keepalive);
-
-    // Set keepalive specific fields
-    if (0 != ack_only)
-        ka->ack_only = (uint32_t)0xFFFFFFFF;
-    else
-        ka->ack_only = (uint32_t)0x00000000;
-
-    // Send packet
-        if (0 != sdxext_send_packet(cxn,
-            (void*)pdu,
-            sizeof(struct sdx_bgp_pdu) - sizeof(struct sdx_bgp_keepalive*),
-            ka,
-            sizeof(struct sdx_bgp_keepalive)))
-    {
-        return -2;
-    }
-
-}
-
-int
-sdxext_build_and_send_update_list (struct sdx_bgp_connection* cxn,
-                                   struct sdx_bgp_update* update_list)
-{
-//TODO
-
-
-
-
-    // Allocate message on connection structure
-
-    // Set generic fields
-
-    // Set update list specific fields
-
-    // Build payload
-
-    // Send packet header
-
-    // Send payload
-
-
-
-}
-
-int
 sdxext_send_packet(struct sdx_bgp_connection* cxn,
-                   void* pointer_one, size_t size_of_pointer_one,
-                   void* pointer_two, size_t size_of_pointer_two)
-
+                   void* data_ptr, size_t size_of_data_ptr)
 {
     size_t nbytes = 0;
-    // Send the first part
-    while (nbytes < size_of_pointer_one)
+    while (nbytes < size_of_data_ptr)
     {
-        send(cxn->socket, pointer_one + nbytes,
-             size_of_pointer_one - nbytes, 0);
-    }
-
-    // if there is a second part, send it
-    nbytes = 0;
-    while (nbytes < size_of_pointer_two)
-    {
-        send(cxn->socket, pointer_two + nbytes,
-             size_of_pointer_two - nbytes, 0);
+      // typecast is to be sure the pointer math works correctly
+      send(cxn->socket, (char*)data_ptr + nbytes,
+             size_of_data_ptr - nbytes, 0);
     }
 }
 
@@ -723,7 +294,7 @@ sdxext_network_thread(void* portnumvoid)
 
     FD_SET(listener, &all_sockets);
     fdmax = listener; // init
-
+/* THIS NEEDS TO BE REWRITTEN.
     // Select loop (a glorious infinite loop! For now, should change this)
     while(1)
     {
@@ -760,8 +331,11 @@ sdxext_network_thread(void* portnumvoid)
                         }
                     }
                 }
+
                 else
                 {
+		    
+		   
                     // We've got data.
                     int nbytes = 0;
                     int headersize = sizeof(struct sdx_bgp_pdu) - sizeof(struct sdx_bgp_login_response*);
@@ -812,71 +386,10 @@ sdxext_network_thread(void* portnumvoid)
             } // FD_ISSET
         } // for i<fdmax
     } // while (1)
+*/
     // TODO: error handling and cleanup. Right now, it's super ugly.
 }
 
-
-int
-sdxext_filter_does_cxn_care_about_route(struct sdx_bgp_connection* cxn,
-                                        char* route)
-{
-    // TODO - What does a route look like? I'm just going with a char* for the
-    // time being just as a place holder.
-    char as_str[12]; // if they're longer than this....
-    int as_str_len;
-    int route_str_len;
-    struct sdx_bgp_as_entry* as_entry;
-
-    route_str_len = strlen(route);
-    as_entry = cxn->as_list_head;
-    while (NULL != as_entry)
-    {
-        // look to see if one of the ones we care about is at the end of the
-        // route
-        as_str_len = sprintf(as_str, " %i", as_entry->entry);
-
-        if (0 == strncmp(as_str,
-                         route + route_str_len - as_str_len,
-                         as_str_len))
-         {
-             return 1;
-         }
-
-        as_entry = as_entry->next;
-    }
-    return 0;
-}
-
-int
-sdxext_filter_from_assegment(struct sdx_bgp_connection* cxn,
-                             struct assegment* segment)
-{
-    // TODO - What does a route look like? I'm just going with a char* for the
-    // time being just as a place holder.
-    struct sdx_bgp_as_entry* as_entry;
-    struct assegment* tempseg = segment;
-
-    if (NULL == segment)
-        return 0;
-
-    // Get to the end of the assegment list
-    while (NULL != tempseg->next)
-    {
-        tempseg = tempseg->next;
-    }
-
-    // go through all the as_entries that are associated with this connection
-    as_entry = cxn->as_list_head;
-    while (NULL != as_entry)
-    {
-        // If the one at the end of the segment is one we care about, return 1
-        if (tempseg->as == as_entry->entry)
-            return 1;
-
-        as_entry = as_entry->next;
-    }
-    return 0;
-}
 
 
 void
@@ -890,84 +403,18 @@ sdxext_new_route_received(char* route)
     {
         if (connection_array[i].in_use)
         {
-            if(sdxext_filter_does_cxn_care_about_route(&connection_array[i],
+	    /* NEED TO REDO
+	    if(sdxext_filter_does_cxn_care_about_route(&connection_array[i],
                                                        route))
             {
-                struct sdx_bgp_update update;
+	        struct sdx_bgp_update update;
                 // TODO - put together update
 
                 // send it on the current connection
                 sdxext_build_and_send_update_list(&connection_array[i],
                                                   &update);
             }
+	    */
         }
     }
-}
-
-int
-sdxext_fetch_all_routes(struct sdx_bgp_connection* cxn,
-                        struct sdx_bgp_update* update_list)
-{
-
-    // based upon, among other functions, peer_lookup(), peer_adj_routes(),
-    // show_adj_routes(), route_vty_out_tmp()
-    struct listnode* bgpnode;
-    struct listnode* nbgpnode;
-    //struct peer* peer;
-    struct bgp* bgp;
-    struct bgp_table* table;
-    struct bgp_node* rn;
-    struct bgp_adj_in* ain;
-    struct prefix* p;
-    struct attr* attr;
-    afi_t afi = AFI_IP;
-    safi_t safi = SAFI_UNICAST;
-    struct sdx_bgp_update* curr_update;
-
-
-
-    for (ALL_LIST_ELEMENTS (bm->bgp, bgpnode, nbgpnode, bgp))
-    {
-        table = bgp->rib[afi][safi];
-
-        for (rn = bgp_table_top(table); NULL == rn; rn = bgp_route_next(rn))
-        {
-            for (ain = rn->adj_in; ain; ain = ain->next)
-            {
-                p = &(rn->p);
-                attr = ain->attr;
-
-                // See if it is something we care about
-                if (sdxext_filter_from_assegment(cxn, attr->aspath->segments))
-                {
-                    struct assegment* segment;
-                    // allocate a new structure and add to list
-                    if (NULL == update_list)
-                    {
-                        update_list = malloc(sizeof(struct sdx_bgp_update));
-                        curr_update = update_list;
-                    }
-                    else
-                    {
-                        curr_update->next =
-                                    malloc(sizeof(struct sdx_bgp_update));
-                        curr_update = curr_update->next;
-                    }
-
-                    // fill in structure
-                    curr_update->next = NULL;
-                    segment = attr->aspath->segments;
-                    while (NULL != segment->next)
-                    {
-                        segment = segment->next;
-                    }
-                    curr_update->as = segment->as;
-                    curr_update->status = SDX_BGP_CURRENT;
-                    curr_update->attr = attr;
-                    curr_update->prefix = p;
-                }
-            }
-        }
-    } // for all BGP sessions(?)
-
 }
--- quagga-0.99.21/bgpd/bgp_sdxext.h
+++ quagga-0.99.21/bgpd/bgp_sdxext.h
@@ -30,8 +30,7 @@
 
 
 // #defines and globals
-#define PROTOCOL_VERSION        1
-#define SUPERSECRETLOGINSECRET  0xDFDFDFDF
+#define PROTOCOL_VERSION 1
 #define NUM_CXNS 1
 #define MISSED_KEEPALIVES 3
 #define KEEPALIVE_TIMEOUT 10
@@ -40,115 +39,13 @@
 
 // Enumerations
 
-enum sdx_bgp_pdu_opcode
-{
-    SDX_BGP_LOGIN_REQUEST       = 0,
-    SDX_BGP_LOGIN_RESPONSE      = 1,
-    SDX_BGP_KEEPALIVE           = 2,
-    SDX_BGP_REGISTER_AS         = 3,
-    SDX_BGP_DEREGISTER_AS       = 4,
-    SDX_BGP_REQUEST_ALL_ROUTES  = 5,
-    SDX_BGP_UPDATE_LIST         = 6,
-
-    // Still need the ones for sending new routes to server.
-};
-
-enum sdx_bgp_UD_or_WD
-{
-    SDX_BGP_UPDATE              = 0,
-    SDX_BGP_WITHDRAWAL          = 1,
-    SDX_BGP_CURRENT             = 2,
-};
 
 
-/* This is used down below, but is not something that's part of the PDU
- * structure, rather part of the "sdx_bgp_udpates" structure.
- */
-struct sdx_bgp_update
-{
-    struct sdx_bgp_update* next;
-    uint32_t as;
-    // Should the source of the update be available? Should it be sent?
-    enum sdx_bgp_UD_or_WD status;
-    //uint32_t size_of_path;
-    //char* path;        // don't forget to free this!
-    struct attr* attr;
-    struct prefix* prefix;
-};
-
-struct sdx_bgp_pdu
-{
-    enum sdx_bgp_pdu_opcode opcode;
-    uint32_t send_sequence;
-    uint32_t recv_sequence;
-    uint32_t connection_id;
-    uint32_t size;              // of the specifics below only!
-
-    union
-    {
-        // Definitions follow
-        struct sdx_bgp_login_request*       login_request;
-        struct sdx_bgp_login_response*      login_response;
-        struct sdx_bgp_keepalive*           keepalive;
-        struct sdx_bgp_register_as*         register_as;
-        struct sdx_bgp_deregister_as*       deregister_as;
-        struct sdx_bgp_request_all_routes*  request_all_routes;
-        struct sdx_bgp_update_list*         update_list;
-    };
-};
-
-struct sdx_bgp_login_request
-{
-    uint32_t version;
-    uint32_t secret;
-};
-
-struct sdx_bgp_login_response
-{
-    uint32_t version;
-    uint32_t secret;
-};
-
-struct sdx_bgp_keepalive
-{
-    uint32_t ack_only;
-};
-
-struct sdx_bgp_register_as
-{
-    uint32_t as;
-};
-
-struct sdx_bgp_deregister_as
-{
-    uint32_t as;
-};
-
-struct sdx_bgp_request_all_routes
-{
-    uint32_t as;
-};
-
-struct sdx_bgp_update_list
-{
-    uint32_t size;
-    struct sdx_bgp_update* update_list;
-
-};
-
-struct sdx_bgp_as_entry
-{
-    struct sdx_bgp_as_entry* prev;
-    int entry;
-    struct sdx_bgp_as_entry* next;
-};
-
-/*
- * Connection related structures
- */
+// Connection related structures
 
 struct sdx_bgp_connection
 {
+    uint32_t in_use;
     uint32_t connection_id;
     uint32_t recv_sequence;
     uint32_t send_sequence;
@@ -163,19 +60,11 @@ struct sdx_bgp_connection
     // connection secret
     // keepalive timer
     timer_t timer;
+    uint32_t missed_keepalives;
     // list of registered ASes?
-    struct sdx_bgp_as_entry* as_list_head;
-    struct sdx_bgp_as_entry* as_list_tail;
     // Watchlist of ASes being monitored
     // PDU pointer for outstanding message?
     // flags
-    uint32_t secret;
-    uint32_t missed_keepalives;
-    struct sdx_bgp_pdu pdu_to_send;
-    void* buffer_to_send;               // Allocated on demand based on pdu type
-    void* update_list_to_send;          // Allocated when sending update lists
-    int in_use;
-
 
 };
 
@@ -228,73 +117,8 @@ int
 sdxext_restart_cxn_timer(struct sdx_bgp_connection* cxn);
 
 int
-sdxext_get_pdu_to_send_resources(struct sdx_bgp_connection* cxn,
-                                 struct sdx_bgp_pdu* pdu,
-                                 size_t pdu_specifics_size,
-                                 void* pdu_specifics,
-                                 size_t update_buffer_size,
-                                 void* update_buffer);
-
-int
-sdxext_free_sent_pdu_resources(struct sdx_bgp_connection* cxn);
-
-
-int
-sdxext_receive_PDU(struct sdx_bgp_pdu* pdu, struct sdx_bgp_connection* cxn);
-
-/*
- * AS list manipulation
- */
-
-int
-sdxext_register_AS(struct sdx_bgp_connection* cxn, int as);
-
-int
-sdxext_deregister_AS(struct sdx_bgp_connection* cxn, int as);
-
-int
-sdxext_is_AS_registered(struct sdx_bgp_connection* cxn, int as);
-
-/*
- * PDU processing functions
- */
-int
-sdxext_process_login_request(struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu);
-
-int
-sdxext_process_keepalive (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu);
-
-int
-sdxext_process_register_as (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu);
-
-int
-sdxext_process_deregister_as (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu);
-
-int
-sdxext_process_request_all_routes (struct sdx_bgp_connection* cxn,
-        struct sdx_bgp_pdu* pdu);
-
-
-
-int
-sdxext_build_and_send_login_response (struct sdx_bgp_connection* cxn);
-
-int
-sdxext_build_and_send_keepalive (struct sdx_bgp_connection* cxn,
-                                 int ack_only);
-
-int
-sdxext_build_and_send_update_list (struct sdx_bgp_connection* cxn,
-                                   struct sdx_bgp_update* update_list);
-
-int
 sdxext_send_packet(struct sdx_bgp_connection* cxn,
-                   void* pointer_one, size_t size_of_pointer_one,
-                   void* pointer_two, size_t size_of_pointer_two);
+                   void* data_ptr, size_t size_of_data_ptr);
 
 /*
  * Network thread
@@ -302,24 +126,6 @@ sdxext_send_packet(struct sdx_bgp_connection* cxn,
 void*
 sdxext_network_thread(void* portnumvoid);
 
-/*
- * Filtering functions
- */
-int
-sdxext_filter_does_cxn_care_about_route(struct sdx_bgp_connection* cxn,
-                                        char* route);
-
-int
-sdxext_filter_from_assegment(struct sdx_bgp_connection* cxn,
-                             struct assegment* segment);
-
-
-/*
- * Fetching all the existing Routes
- */
-int
-sdxext_fetch_all_routes(struct sdx_bgp_connection* cxn,
-                        struct sdx_bgp_update* update_list);
 
 #endif // __SDXEXT__
 
